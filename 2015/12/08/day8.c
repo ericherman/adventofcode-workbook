2d8bf7abca70799c23e0b37f84c52b00522ec369
Eric Herman <eric@freesa.org>
2016-03-05 21:40:02 +0100

    http://adventofcode.com 2015 day 8 part 2

--- 2015/12/08/day8.c
+++ 2015/12/08/day8.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 	FILE *input;
 	char buf[255];
 	size_t i;
-	unsigned total, chars, escaped, instr;
+	unsigned total, chars, escaped, instr, double_encode;
 
 	path = (argc > 1) ? argv[1] : "input";
 	input = fopen(path, "r");
@@ -19,14 +19,25 @@ int main(int argc, char **argv)
 	chars = 0;
 	escaped = 0;
 	instr = 0;
+	double_encode = 0;
 	while (fgets(buf, 255, input)) {
 		for (i = 0; buf[i] != '\0' && i < 255; ++i) {
 			if (!instr) {
 				if (buf[i] == '"') {
 					instr = 1;
 					++total;
+					double_encode += 4;
 				}
 			} else {
+				switch (buf[i]) {
+				case '"':
+				case '\\':
+					double_encode += 2;
+					break;
+				default:
+					++double_encode;
+					break;
+				}
 				++total;
 				if (escaped) {
 					++chars;
@@ -35,6 +46,7 @@ int main(int argc, char **argv)
 					escaped = 1;
 					if (buf[i + 1] == 'x') {
 						i += 2;
+						double_encode += 2;
 						total += 2;
 					}
 				} else if (buf[i] == '"') {
@@ -48,7 +60,8 @@ int main(int argc, char **argv)
 
 	fclose(input);
 
-	printf("total: %u, mem chars:%u \n", total, chars);
+	printf("total: %u, mem chars:%u (double_encode: %u)\n", total, chars,
+	       double_encode);
 
 	return 0;
 }
