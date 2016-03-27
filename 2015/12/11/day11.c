a6c3bcfd8bf72cfb89d280995fe20bfdb5f32972
Eric Herman <eric@freesa.org>
2016-03-27 11:34:45 +0200

    http://adventofcode.com 2015 day 11

---
+++ 2015/12/11/day11.c
@@ -0,0 +1,108 @@
+#include <stdio.h>		/* printf */
+#include <stdlib.h>		/* atoi */
+#include <string.h>		/* strlen strcpy */
+
+#define PW_LEN 8
+#define BUF_LEN (PW_LEN + 1)
+
+static void inc_buf(char *c, size_t len)
+{
+	size_t i = len;
+
+	while (i) {
+		--i;
+		c[i]++;
+		switch (c[i]) {
+		case ('z' + 1):
+			c[i] = 'a';
+			break;
+		case 'i':
+		case 'o':
+		case 'l':
+			c[i]++;
+		default:
+			return;
+		}
+	}
+}
+
+int contains_3_letter_strait(char *c, size_t len, int print)
+{
+	size_t i;
+
+	for (i = 0; i < len - 2; ++i) {
+		if (((c[i] + 1) == c[i + 1]) && ((c[i] + 2) == c[i + 2])) {
+			if (print) {
+				printf("%c%c%c\n", c[i], c[i + 1], c[i + 2]);
+			}
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int contains_2_non_overlapping_pairs(char *c, size_t len, int print)
+{
+	size_t i, pairs;
+
+	pairs = 0;
+	for (i = 0; i < len - 1; ++i) {
+		if (c[i] == c[i + 1]) {
+			++pairs;
+			if (print) {
+				printf("%c%c", c[i], c[i + 1]);
+			}
+			if (pairs == 2) {
+				if (print) {
+					printf("\n");
+				}
+				return 1;
+			}
+			++i;
+		}
+	}
+	if (pairs && print) {
+		printf("\n");
+	}
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	const char *input;
+	char buf[BUF_LEN];
+	size_t len, verbose;
+
+	verbose = (argc > 1) ? atoi(argv[1]) : 0;
+	input = (argc > 2) ? argv[2] : "cqjxjnds";
+
+	len = strlen(input);
+	if (len > PW_LEN) {
+		fprintf(stderr, "input too long\n");
+		return 1;
+	} else if (len == 0) {
+		fprintf(stderr, "empty string\n");
+		return 1;
+	}
+
+	strcpy(buf, input);
+	if (verbose) {
+		printf("%s\n", buf);
+	}
+
+	for (;;) {
+		inc_buf(buf, len);
+		if (verbose) {
+			printf("%s\n", buf);
+		}
+		if (contains_3_letter_strait(buf, len, verbose)
+		    && contains_2_non_overlapping_pairs(buf, len, verbose)) {
+			break;
+		}
+	}
+
+	printf("%s\n", buf);
+	contains_3_letter_strait(buf, len, 1);
+	contains_2_non_overlapping_pairs(buf, len, 1);
+	return 0;
+}
