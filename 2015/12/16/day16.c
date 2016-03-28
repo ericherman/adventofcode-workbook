4d156f728f6f320a29a8a72e30f71b0e1be94a43
Eric Herman <eric@freesa.org>
2016-03-28 22:30:42 +0200

    http://adventofcode.com 2015 day 16 part 2

--- 2015/12/16/day16.c
+++ 2015/12/16/day16.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct triaits_s {
 	const char *key;
 	const int val;
+	char mode;
 };
 
 int main(int argc, char **argv)
@@ -12,26 +14,33 @@ int main(int argc, char **argv)
 	FILE *input;
 	char buf[255];
 	char s[3][40];
-	int i, j, m, n, v[3], wrong;
+	int i, j, m, n, val, v[3], wrong, magnetoreluctance;
 
 	struct triaits_s traits[] = {
-		{"children:", 3},
-		{"cats:", 7},
-		{"samoyeds:", 2},
-		{"pomeranians:", 3},
-		{"akitas:", 0},
-		{"vizslas:", 0},
-		{"goldfish:", 5},
-		{"trees:", 3},
-		{"cars:", 2},
-		{"perfumes:", 1},
+		{"children:", 3, '='},
+		{"cats:", 7, '>'},
+		{"samoyeds:", 2, '='},
+		{"pomeranians:", 3, '<'},
+		{"akitas:", 0, '='},
+		{"vizslas:", 0, '='},
+		{"goldfish:", 5, '<'},
+		{"trees:", 3, '>'},
+		{"cars:", 2, '='},
+		{"perfumes:", 1, '='},
 	};
 
-	input_path = (argc > 1) ? argv[1] : "input";
+	magnetoreluctance = (argc > 1) ? atoi(argv[1]) : 0;
+	input_path = (argc > 2) ? argv[2] : "input";
 	input = fopen(input_path, "r");
 	if (!input) {
 		fprintf(stderr, "could not open %s\n", input_path);
 		return 1;
+	}
+
+	if (!magnetoreluctance) {
+		for (j = 0; j < 10; ++j) {
+			traits[j].mode = '\0';
+		}
 	}
 
 	in = "Sue %d: %40s %d, %40s %d, %40s %d";
@@ -44,8 +53,23 @@ int main(int argc, char **argv)
 			for (i = 0; i < 3; ++i) {
 				for (j = 0; j < 10; ++j) {
 					if (strcmp(traits[j].key, s[i]) == 0) {
-						if (traits[j].val != v[i]) {
-							wrong++;
+						val = traits[j].val;
+						switch (traits[j].mode) {
+						case '>':
+							if (val >= v[i]) {
+								wrong++;
+							}
+							break;
+						case '<':
+							if (val <= v[i]) {
+								wrong++;
+							}
+							break;
+						case '=':
+						default:
+							if (val != v[i]) {
+								wrong++;
+							}
 						}
 					}
 				}
