8bd848dc2893962d25ba1d81a49c36c3a83acb29
Eric Herman <eric@freesa.org>
2016-05-17 21:10:38 +0200

    support c89, no "unsigned long long": libehbigint
    https://github.com/ericherman/libehbigint
    
    But use c99, libehbigint is REALLY SLOW:
    
    eric@titan:~/src/adventofcode-workbook/2015/12/25$ time { ./day25 ; }
    row: 3010, col: 3019
    pos:18168397
       |    1         2         3         4         5         6
    ---+---------+---------+---------+---------+---------+---------+
     1 | 20151125  18749137  17289845  30943339  10071777  33511524
     2 | 31916031  21629792  16929656   7726640  15514188   4041754
     3 | 16080970   8057251   1601130   7981243  11661866  16474243
     4 | 24592653  32451966  21345942   9380097  10600672  31527494
     5 |    77061  17552253  28094349   6899651   9250759  31663883
     6 | 33071741   6796745  25397450  24659492   1534922  27995004
      18168384 of 18168397
    8997277
    
    real	11236m12.918s
    user	11226m55.572s
    sys	1m36.704s
    eric@titan:~/src/adventofcode-workbook/2015/12/25$
    
    But with c99 is is done in moments:
    
    eric@titan:~/src/adventofcode-workbook/2015/12/25$ time { ./day25 ; }
    8997277
    
    real	0m0.458s
    user	0m0.456s
    sys	0m0.000s
    eric@titan:~/src/adventofcode-workbook/2015/12/25$

--- 2015/12/25/day25.c
+++ 2015/12/25/day25.c
@@ -1,14 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <ehbigint.h>
 
-static unsigned long long mulull(unsigned long long x, unsigned long long y)
+#define DIE(format, arg) \
+  fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+  fprintf(stderr, format, arg); \
+  exit(EXIT_FAILURE)
+
+#define DIE2(format, arg1, arg2) \
+  fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+  fprintf(stderr, format, arg1, arg2); \
+  exit(EXIT_FAILURE)
+
+#define DIE3(format, arg1, arg2, arg3) \
+  fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+  fprintf(stderr, format, arg1, arg2, arg3); \
+  exit(EXIT_FAILURE)
+
+#define DIE4(format, arg1, arg2, arg3, arg4) \
+  fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+  fprintf(stderr, format, arg1, arg2, arg3, arg4); \
+  exit(EXIT_FAILURE)
+
+#ifdef __STDC_VERSION__
+#if __STDC_VERSION__ >= 199901L
+#define ULONGEST unsigned long long
+#define ULONGESTFMT "lld"
+#define DEFAULT_VERBOSE 0
+#define SPEED 4096
+#endif
+#endif
+#ifndef ULONGEST
+#define ULONGEST unsigned long
+#define ULONGESTFMT "ld"
+#define DEFAULT_VERBOSE 1
+#define SPEED 16
+#endif
+
+#define BILEN 10
+struct context_s {
+	ULONGEST inmul;
+	ULONGEST inmod;
+	struct ehbigint in;
+	unsigned char in_bytes[BILEN];
+	struct ehbigint mul;
+	unsigned char mul_bytes[BILEN];
+	struct ehbigint res;
+	unsigned char res_bytes[BILEN];
+	struct ehbigint mod;
+	unsigned char mod_bytes[BILEN];
+	struct ehbigint quot;
+	unsigned char quot_bytes[BILEN];
+	struct ehbigint rem;
+	unsigned char rem_bytes[BILEN];
+	int verbose;
+};
+
+static void init_context(struct context_s *ctx, ULONGEST inmul, ULONGEST inmod,
+			 int verbose)
 {
-	unsigned long long result;
-	result = x * y;
-	if (x != 0 && result / x != y) {
-		fprintf(stderr, "%llu * %llu overflows\n", x, y);
-		exit(EXIT_FAILURE);
+	ctx->inmul = inmul;
+	ctx->inmod = inmod;
+
+	ctx->mul.bytes = ctx->mul_bytes;
+	ctx->mod.bytes = ctx->mod_bytes;
+	ctx->quot.bytes = ctx->quot_bytes;
+	ctx->rem.bytes = ctx->rem_bytes;
+	ctx->in.bytes = ctx->in_bytes;
+	ctx->res.bytes = ctx->res_bytes;
+
+	ctx->mul.bytes_len = BILEN;
+	ctx->mod.bytes_len = BILEN;
+	ctx->quot.bytes_len = BILEN;
+	ctx->rem.bytes_len = BILEN;
+	ctx->in.bytes_len = BILEN;
+	ctx->res.bytes_len = BILEN;
+
+	ctx->mul.bytes_used = 0;
+	ctx->mod.bytes_used = 0;
+	ctx->quot.bytes_used = 0;
+	ctx->rem.bytes_used = 0;
+	ctx->in.bytes_used = 0;
+	ctx->res.bytes_used = 0;
+
+	ehbi_set_ul(&ctx->in, 0);
+	ehbi_set_ul(&ctx->mul, ctx->inmul);
+	ehbi_set_ul(&ctx->res, 0);
+	ehbi_set_ul(&ctx->mod, ctx->inmod);
+	ehbi_set_ul(&ctx->quot, 0);
+	ehbi_set_ul(&ctx->rem, 0);
+
+	ctx->verbose = verbose;
+}
+
+static ULONGEST mul_mod(struct context_s *ctx, size_t i, ULONGEST x)
+{
+	ULONGEST result;
+	int err, base;
+	char dec[80];
+	char *endptr;
+
+	result = x * ctx->inmul;
+	if ((x != 0) && ((result / x) == ctx->inmul)) {
+		result = result % ctx->inmod;
+		return result;
 	}
+
+	if (ctx->verbose > 1) {
+		fprintf(stderr,
+			"mul_mod((%10lu): %10" ULONGESTFMT ", %10" ULONGESTFMT
+			", %10" ULONGESTFMT ")\n", (unsigned long)i, x,
+			ctx->inmul, ctx->inmod);
+	}
+
+	err = ehbi_set_ul(&ctx->in, x);
+	if (err) {
+		DIE("ehbi_set_ul: %d\n", err);
+	}
+
+	if (ctx->verbose > 3) {
+		printf("mul start\n");
+	}
+	err = ehbi_mul(&ctx->res, &ctx->in, &ctx->mul);
+	if (ctx->verbose > 3) {
+		printf("mul done\n");
+	}
+	if (err) {
+		DIE("ehbi_mul: %d\n", err);
+	}
+	if (ctx->verbose > 2) {
+		err = ehbi_to_decimal_string(&ctx->in, dec, 80);
+		if (err) {
+			DIE3("%d from ehbi_to_decimal_string(%p, %p, 80)\n",
+			     err, (void *)&ctx->in, (void *)dec);
+		}
+		printf("%s", dec);
+		err = ehbi_to_decimal_string(&ctx->mul, dec, 80);
+		if (err) {
+			DIE3("%d from ehbi_to_decimal_string(%p, %p, 80)\n",
+			     err, (void *)&ctx->mul, (void *)dec);
+		}
+		printf(" * %s = ", dec);
+		err = ehbi_to_decimal_string(&ctx->res, dec, 80);
+		if (err) {
+			DIE3("%d from ehbi_to_decimal_string(%p, %p, 80)\n",
+			     err, (void *)&ctx->res, (void *)dec);
+		}
+		printf("%s\n", dec);
+		printf("ehbi_div(&cxt->quot, &ctx->rem, %s,", dec);
+		err = ehbi_to_decimal_string(&ctx->mod, dec, 80);
+		if (err) {
+			DIE3("%d from ehbi_to_decimal_string(%p, %p, 80)\n",
+			     err, (void *)&ctx->mod, (void *)dec);
+		}
+		printf("%s)\n", dec);
+	}
+
+	err = ehbi_div(&ctx->quot, &ctx->rem, &ctx->res, &ctx->mod);
+	if (ctx->verbose > 3) {
+		printf("done\n");
+	}
+	if (err) {
+		DIE("ehbi_div: %d\n", err);
+	}
+	if ((ctx->rem.bytes_used > (1 + sizeof(unsigned long)))
+	    || ((ctx->rem.bytes_used == (1 + sizeof(unsigned long)))
+		&& (ctx->rem.bytes[0] != 0x00))) {
+		DIE("bx.bytes_used > sizeof(unsigned long)(%lu)\n",
+		    (unsigned long)ctx->rem.bytes_used);
+	}
+
+	err = ehbi_to_decimal_string(&ctx->rem, dec, 80);
+	if (err) {
+		DIE("%d from ehbi_to_decimal_string\n", err);
+	}
+
+	base = 10;
+	errno = 0;
+	result = strtoul(dec, &endptr, base);
+	err = errno;
+	if (err || (endptr && strlen(endptr) > 0)) {
+		DIE4("strtoul('%s'); (invalid:'%s') (%d: %s)\n", dec, endptr,
+		     err, strerror(err));
+	}
+
 	return result;
 }
 
@@ -32,11 +209,14 @@ int main(int argc, char **argv)
 {
 	int verbose;
 	size_t row, col, x, y, i, pos;
-	unsigned long long first, mul, mod, next;
+	ULONGEST first, next;
+	struct context_s ctx;
 
-	verbose = (argc > 1) ? atoi(argv[1]) : 0;
+	verbose = (argc > 1) ? atoi(argv[1]) : DEFAULT_VERBOSE;
 	row = (argc > 2) ? atoi(argv[2]) : 0;
 	col = (argc > 3) ? atoi(argv[3]) : 0;
+
+	init_context(&ctx, 252533, 33554393, verbose);
 
 	if (row == 0) {
 		row = 3010;
@@ -48,16 +228,15 @@ int main(int argc, char **argv)
 	pos = find_pos(col, row);
 
 	first = 20151125;
-	mul = 252533;
-	mod = 33554393;
 
 	next = first;
 
-	if (verbose) {
+	if (ctx.verbose) {
 		printf("row: %lu, col: %lu\n", (unsigned long)row,
 		       (unsigned long)col);
 		printf("pos:%lu\n", (unsigned long)pos);
-
+	}
+	if (ctx.verbose) {
 		printf("   |");
 		for (x = 1; x <= 6; ++x) {
 			printf("    %u     ", x);
@@ -68,34 +247,37 @@ int main(int argc, char **argv)
 			printf("---------+");
 		}
 		printf("\n");
+		fflush(stdout);
 		for (y = 1; y <= 6; ++y) {
 			printf(" %u |", y);
 			for (x = 1; x <= 6; ++x) {
 				pos = find_pos(x, y);
 				next = first;
 				for (i = 1; i < pos; ++i) {
-					if (verbose > 1) {
-						next = mulull(next, mul) % mod;
-					} else {
-						next = (next * mul) % mod;
-					}
+					next = mul_mod(&ctx, i, next);
 				}
-				printf("%9llu ", next);
+				printf("%9" ULONGESTFMT " ", next);
 			}
 			printf("\n");
+			fflush(stdout);
 		}
 	}
+	fflush(stdout);
 	pos = find_pos(col, row);
 	next = first;
 	for (i = 1; i < pos; ++i) {
-		if (verbose > 1) {
-			next = mulull(next, mul) % mod;
-		} else {
-			next = (next * mul) % mod;
+		next = mul_mod(&ctx, i, next);
+		if ((ctx.verbose == 1) && ((i % SPEED) == 0)) {
+			printf("\r%10lu of %lu ", (unsigned long)i,
+			       (unsigned long)pos);
+			fflush(stdout);
 		}
 	}
+	if (ctx.verbose) {
+		printf("\n");
+	}
 
-	printf("%llu\n", next);
+	printf("%" ULONGESTFMT "\n", next);
 
 	return 0;
 }
