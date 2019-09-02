53afd039e7f27a491515e18debc4f2804d746f0e
Eric Herman <eric@freesa.org>
2019-09-02 12:14:17 +0200

    whitespace tidy

--- 2015/12/16/day16.c
+++ 2015/12/16/day16.c
@@ -27,16 +27,16 @@ int main(int argc, char **argv)
 	int i, j, m, n, val, v[3], wrong, magnetoreluctance;
 
 	struct triaits_s traits[] = {
-		{"children:", 3, '='},
-		{"cats:", 7, '>'},
-		{"samoyeds:", 2, '='},
-		{"pomeranians:", 3, '<'},
-		{"akitas:", 0, '='},
-		{"vizslas:", 0, '='},
-		{"goldfish:", 5, '<'},
-		{"trees:", 3, '>'},
-		{"cars:", 2, '='},
-		{"perfumes:", 1, '='},
+		{ "children:", 3, '=' },
+		{ "cats:", 7, '>' },
+		{ "samoyeds:", 2, '=' },
+		{ "pomeranians:", 3, '<' },
+		{ "akitas:", 0, '=' },
+		{ "vizslas:", 0, '=' },
+		{ "goldfish:", 5, '<' },
+		{ "trees:", 3, '>' },
+		{ "cars:", 2, '=' },
+		{ "perfumes:", 1, '=' },
 	};
 
 	input_path = (argc > 1) ? argv[1] : "input";
